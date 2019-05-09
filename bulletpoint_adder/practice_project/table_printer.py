98166990e9561fa8f671d2f1a7e6b64e84970dc9
superzaky <superkytoz@gmail.com>
2019-05-09 18:51:17 +0200

    Refactor change names.

--- bulletpoint_adder/practice_project/table_printer.py
+++ bulletpoint_adder/practice_project/table_printer.py
@@ -1,24 +1,22 @@
-def column_wideth(tabel):
-    colWideths = [0] * len(tabel)
+def column_width(tabel):
+    columnWidths = [0] * len(tabel)
     for i in range(len(tabel)):
         for j in range(len(tabel[i])):
-            if len(tabel[i][j]) > colWideths[i]:
-                colWideths[i] = len(tabel[i][j])
-    return colWideths
+            if len(tabel[i][j]) > columnWidths[i]:
+                columnWidths[i] = len(tabel[i][j])
+    return columnWidths
 
-
-def print_table(table, colWideths):
+def print_table(table, columnWidths):
     for i in range(len(table[0])):  # i = 4 meansing the output has 4 rows
         #print (i)
-        for j in range(len(table)):  # j = 3 to output each row
-            print(table[j][i].rjust(colWideths[j]), end=' ')
+        for j in range(len(table)):  # j = 3 to output each column
+            print(table[j][i].rjust(columnWidths[j]), end=' ')
         print()
-
 
 if __name__ == '__main__':
     tableData = [['apples', 'oranges', 'cherries', 'banana'],
                  ['Alice', 'Bob', 'Carol', 'David'],
                  ['dogs', 'cats', 'moose', 'goose']]
 
-    colWideths = column_wideth(tableData)
-    print_table(tableData, colWideths)
+    columnWidths = column_width(tableData)
+    print_table(tableData, columnWidths)
