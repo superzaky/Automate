e5475c86fe67abf0cff66afdb02b85b57219af71
superzaky <superkytoz@gmail.com>
2019-12-02 20:52:33 +0100

    Looking busy app.

---
+++ chap_18/looking_busy.py
@@ -0,0 +1,13 @@
+"""Nudges the mouse so messaging programs don't go into idle mode."""
+
+import time
+import pyautogui
+
+print('NudgeBot activated, press CTRL-C to quit.')
+try:
+    while True:
+        pyautogui.moveRel(10, 0, 0.5)
+        pyautogui.moveRel(-10, 0, 0.5)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print(' NudgeBot deactivated.')
